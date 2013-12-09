74cb3bb6a04f5b7bb85b81d700332f2c4cf232aa
Andrew Chilton <andychilton@gmail.com>
2013-12-09 22:11:39 +1300

    Convert the output to numbers, not strings

--- index.js
+++ index.js
@@ -13,23 +13,23 @@ var exec = require('child_process').exec;
 
 // ----------------------------------------------------------------------------
 
-module.exports = function(options, callback) {
+function convertToInt(field) {
+    field = field.substr(0, field.length-1);
+    return parseInt(field, 10);
+}
+
+module.exports = function(opts, callback) {
     // just get the df stats for this device
-    options.unit = options.unit || 'G';
-    exec('df -B' + options.unit + ' ' + options.device + ' | tail -1', function (err, stdout, stderr) {
+    opts.unit = opts.unit || 'G';
+    exec('df -B' + opts.unit + ' ' + opts.device + ' | tail -1', function (err, stdout, stderr) {
         if (err) return callback(err);
         
-        console.log(stdout);
         var fields = stdout.split(/\s+/);
-        console.log(fields);
-
         var data = {
-            used  : fields[2].substr(0, fields[2].length-1),
-            free  : fields[3].substr(0, fields[3].length-1),
-            total : fields[1].substr(0, fields[1].length-1),
+            used  : convertToInt(fields[2]),
+            free  : convertToInt(fields[3]),
+            total : convertToInt(fields[1]),
         };
-        console.log(data)
-
         callback(null, data);
     });
 
